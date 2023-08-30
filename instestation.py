8f58cc2814a2b517150fe9ed138bd2bdeb94ca50
Looker <129039795+looker101@users.noreply.github.com>
2023-08-30 15:26:24 +0200

    Create instestation.py

---
+++ instestation.py
@@ -0,0 +1,7 @@
+# instestazione per creare il dataframe relativo alla sitemap
+import pandas as pd
+
+df = pd.read_csv(".csv", usecols=["Indirizzo", "Codice di stato", "Indicizzabilità", "Titolo 1", "Lunghezza del titolo 1", "Meta description 1", "Lunghezza della meta description 1", "H1-1", "H2-1", "H2-2", "Elemento di link canonical 1", "Conteggio delle parole", "Conteggio delle frasi", "Leggibilità", "Rapporto di testo", "Livello di scansione", "Link in entrata", "Link in entrata unici", "Link in uscita", "Link in uscita unici", "Tempo di risposta", "Indirizzo URL codificato"], index_col="Indirizzo")
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_colwidth", None)
+df.head()
