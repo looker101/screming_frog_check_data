ea03853cff21a76dfcd6d142c62f6e816e2e0233
looker101 <lookeronline@gmail.com>
2023-10-16 17:01:01 +0200

    modificato il nome della colonna dei breadcrumbs link e di testo

--- checkBreadcrumbs.py
+++ checkBreadcrumbs.py
@@ -6,24 +6,24 @@ class Breadcrumbs:
         self.brand = None  # Definisci brand come attributo della classe
 
     def getFile(self):
-        self.breadcrumbs = pd.read_csv("estrazione_personalizzata_tutti.csv", usecols=["Indirizzo", "html 1", "text 1"])
+        self.breadcrumbs = pd.read_csv("adidas.csv", usecols=["Indirizzo", "Bread_HTML 1", "Bread_TEXT 1"])
         # formatto le colonne in modo da poterle confrontare
-        self.breadcrumbs["html 1"] = self.breadcrumbs["html 1"].str.replace("\r\n", " ").str.replace("</span>", " ").str.replace("<span>", " ")
-        self.breadcrumbs["text 1"] = self.breadcrumbs["text 1"].str.replace("\n", " ")
+        self.breadcrumbs["Bread_HTML 1"] = self.breadcrumbs["Bread_HTML 1"].str.replace("\r\n", " ").str.replace("</span>", " ").str.replace("<span>", " ")
+        self.breadcrumbs["Bread_TEXT 1"] = self.breadcrumbs["Bread_TEXT 1"].str.replace("\n", " ")
         # tolgo eventuali spazi dalle righe
-        self.breadcrumbs["html 1"] = self.breadcrumbs["html 1"].str.strip()
+        self.breadcrumbs["Bread_HTML 1"] = self.breadcrumbs["Bread_HTML 1"].str.strip()
         self.breadcrumbs.dropna(inplace=True)
         return self.breadcrumbs
 
     def getBrand(self):
         self.brand = str(input("Scegli il brand a cui vuoi controllare i Breadcrumbs: "))  # Assegna il valore a brand
-        mask = self.breadcrumbs["html 1"].str.contains(self.brand, case=False)
+        mask = self.breadcrumbs["Bread_HTML 1"].str.contains(self.brand, case=False)
         bread = self.breadcrumbs[mask]
         return bread
     
     def getCheck(self):
-        file["_link"] = file["html 1"].str.split(">").str[-1].str.strip()
-        file["breadcrumb"] = file["text 1"].str.split(">").str[-1].str.strip()
+        file["_link"] = file["Bread_HTML 1"].str.split(">").str[-1].str.strip()
+        file["breadcrumb"] = file["Bread_TEXT 1"].str.split(">").str[-1].str.strip()
         file["Check"] = file["_link"] == file["breadcrumb"]
         return file
         
