2ac3bc9be743332c42a60bd430a7a3858f507425
looker101 <lookeronline@gmail.com>
2023-10-16 14:39:54 +0200

    corretto il file breadcrumbs

--- checkBreadcrumbs.py
+++ checkBreadcrumbs.py
@@ -3,24 +3,26 @@ import pandas as pd
 class Breadcrumbs:
     def __init__(self):
         self.breadcrumbs = None
-        
+        self.brand = None  # Definisci brand come attributo della classe
+
     def getFile(self):
-        self.breadcrumbs = pd.read_csv("amq_Balenciaga_bottega.csv", usecols = ["Indirizzo", "Bread_HTML 1", "Bread_TEXT 1"])
+        self.breadcrumbs = pd.read_csv("estrazione_personalizzata_tutti.csv", usecols=["Indirizzo", "html 1", "text 1"])
         # formatto le colonne in modo da poterle confrontare
-        self.breadcrumbs["Bread_HTML 1"] = self.breadcrumbs["Bread_HTML 1"].str.replace("\r\n", " ").str.replace("</span>", " ").str.replace("<span>", " ")
-        self.breadcrumbs["Bread_TEXT 1"] = self.breadcrumbs["Bread_TEXT 1"].str.replace("\n", " ")
+        self.breadcrumbs["html 1"] = self.breadcrumbs["html 1"].str.replace("\r\n", " ").str.replace("</span>", " ").str.replace("<span>", " ")
+        self.breadcrumbs["text 1"] = self.breadcrumbs["text 1"].str.replace("\n", " ")
         # tolgo eventuali spazi dalle righe
-        self.breadcrumbs["Bread_HTML 1"] = self.breadcrumbs["Bread_HTML 1"].str.strip()
-        self.breadcrumbs.dropna(inplace = True)
+        self.breadcrumbs["html 1"] = self.breadcrumbs["html 1"].str.strip()
+        self.breadcrumbs.dropna(inplace=True)
         return self.breadcrumbs
-    
+
     def getBrand(self):
-        brand = str(input("Scegli il brand a cui vuoi controllare i Breadcrumbs: "))
-        mask = self.breadcrumbs["Bread_HTML 1"].str.contains(brand, case = False)
+        self.brand = str(input("Scegli il brand a cui vuoi controllare i Breadcrumbs: "))  # Assegna il valore a brand
+        mask = self.breadcrumbs["html 1"].str.contains(self.brand, case=False)
         bread = self.breadcrumbs[mask]
         return bread
-                
+
 f = Breadcrumbs()
 
 f.getFile()
-print(f.getBrand())
+file = f.getBrand()
+file.to_csv(f.brand + '.csv')  # Usa f.brand invece di brand
