45aac4306a90d1e4ec56e3a520109ce75074eb96
Looker <129039795+looker101@users.noreply.github.com>
2024-01-24 09:04:52 +0100

    Update sitemap_df_instestation.py

--- sitemap_df_instestation.py
+++ sitemap_df_instestation.py
@@ -1,7 +1,12 @@
-# instestazione per creare il dataframe relativo alla sitemap
+# intenstazione per creare il DF relativo alla sitemap
 import pandas as pd
 
-df = pd.read_csv(".csv", usecols=["Indirizzo", "Codice di stato", "Indicizzabilità", "Titolo 1", "Lunghezza del titolo 1", "Meta description 1", "Lunghezza della meta description 1", "H1-1", "H2-1", "Elemento di link canonical 1", "Conteggio delle parole", "Conteggio delle frasi", "Leggibilità", "Rapporto di testo", "Livello di scansione", "Link in entrata", "Link in entrata unici", "Link in uscita", "Link in uscita unici", "Tempo di risposta", "Indirizzo URL codificato"], index_col="Indirizzo")
+df = pd.read_csv("filname.csv", usecols=[
+  "Indirizzo", "Codice di stato", "Indicizzabilità", "Titolo 1", "Lunghezza del titolo 1", "Meta description 1", 
+  "Lunghezza della meta description 1", "H1-1", "H2-1", "Elemento di link canonical 1", "Conteggio delle parole", "Conteggio delle frasi", "Leggibilità", 
+  "Rapporto di testo", "Livello di scansione", "Link in entrata", "Link in entrata unici", "Link in uscita", 
+  "Link in uscita unici", "Tempo di risposta", "Indirizzo URL codificato"
+], index_col="Indirizzo")
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_colwidth", None)
 df.head()
